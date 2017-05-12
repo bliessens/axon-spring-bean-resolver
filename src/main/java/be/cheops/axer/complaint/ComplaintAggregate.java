7b535514322ae7febd88209bc52dd06cb3e4eed1
Benoit Liessens <benoni29@gmail.com>
2017-05-12 11:44:38 +0200

    fix class references

--- src/main/java/be/cheops/axer/complaint/ComplaintAggregate.java
+++ src/main/java/be/cheops/axer/complaint/ComplaintAggregate.java
@@ -1,11 +1,12 @@
 package be.cheops.axer.complaint;
 
-import be.cheops.axer.api.postprescription.PurchaseReferredToEvent;
-import be.cheops.axer.api.postprescription.RegisterComplaintCommand;
 import be.cheops.axer.api.postprescription.ComplaintRegisteredEvent;
+import be.cheops.axer.api.postprescription.PurchaseReferredToEvent;
 import be.cheops.axer.api.postprescription.ReferToPurchaseCommand;
+import be.cheops.axer.api.postprescription.RegisterComplaintCommand;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.common.Assert;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
@@ -25,14 +26,14 @@ class ComplaintAggregate {
     }
 
     @CommandHandler
-    public ComplaintAggregate(RegisterComplaintCommand command /*,ParentOrderResolver resolver*/) {
-//        Assert.notNull(resolver, () -> "Requires " + RecipientResolver.class.getSimpleName());
+    public ComplaintAggregate(RegisterComplaintCommand command /*,CustomerResolver resolver*/) {
+//        Assert.notNull(resolver, () -> "Requires " + CustomerResolver.class.getSimpleName());
         apply(new ComplaintRegisteredEvent(command.getAggregateIdentifier()));
     }
 
     @CommandHandler
-    public void handle(ReferToPurchaseCommand command /*, RecipientResolver resolver*/) {
-//        Assert.notNull(resolver, () -> "Requires " + RecipientResolver.class.getSimpleName());
+    public void handle(ReferToPurchaseCommand command/*, PurchaseResolver resolver*/) {
+//        Assert.notNull(resolver, () -> "Requires " + PurchaseResolver.class.getSimpleName());
         apply(new PurchaseReferredToEvent(command.getAggregateIdentifier(), command.getPurchaseId()));
     }
 
